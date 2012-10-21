0202fd1a06eb5b053c0649924cd572d67b7fee9e
thom <thomkim@gmail.com>
2012-10-21 11:00:48 -0700

    Move sample run view and controller code to sample app

--- sample-app/app/controllers/Application.scala
+++ sample-app/app/controllers/Application.scala
@@ -6,7 +6,12 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.run())
+  }
+  
+  def wsActor = WebSocket.async[String] {
+    request =>
+      RunActor.join()
   }
   
 }
