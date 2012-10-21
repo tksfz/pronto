f9b19e2c6d2b82ea40763434358fa0c9140c81cd
thom <thomkim@gmail.com>
2012-10-21 10:59:31 -0700

    Add dependency from sample to module

--- sample-app/project/Build.scala
+++ sample-app/project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "org.tksfz" %% "pronto" % "0.1-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      resolvers += "Local Play Repo" at "file://home/thom/play-2.0.3/repository/local"
     )
 
 }
